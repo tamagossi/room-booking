b4975abd76c37cfa634c90357287599f198ae6c5
Raka Pratama <mgf.prauliyatama@gmail.com>
2024-05-01 22:40:13 +0700

    feat(3.24): checking error

--- main.go
+++ main.go
@@ -11,6 +11,32 @@ func AboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Thjis is the about page!")
 }
 
+/*
+This function used in lecture 3.24 | checking error
+
+func divideValue(x, y float64) (float64, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+
+	result := x / y
+	return result, nil
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	x := 2.0
+	y := 3.0
+
+	f, err := divideValue(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, f))
+}
+*/
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "This is the homepage!")
 }
@@ -18,6 +44,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/about", AboutHandler)
+	// http.HandleFunc("/divide", Divide) // used in lecture 3.24 | checking error
 
 	http.ListenAndServe(port, nil)
 }
