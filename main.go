c4a021b7846a46a103fe55063cab133b4b95e1af
Raka Pratama <mgf.prauliyatama@gmail.com>
2024-05-01 22:31:32 +0700

    feat(3.23): adding handler function

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const port = ":8080"
+
+func AboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Thjis is the about page!")
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is the homepage!")
+}
+
+func main() {
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/about", AboutHandler)
+
+	http.ListenAndServe(port, nil)
+}
