0be060ab78c1ce90670f0395ba0413b878926ba6
Raka Pratama <mgf.prauliyatama@gmail.com>
2024-05-02 22:37:32 +0700

    feat(3.31): enhancing cache generator

--- pkg/utils/render.go
+++ pkg/utils/render.go
@@ -1,14 +1,19 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
 var templateCache = make(map[string]*template.Template)
 
+/*
+	Used in 3.30
+*/
 func createTemplateCache(templateName string) error {
 	templates := []string{
 		fmt.Sprintf("./templates/%s", templateName),
@@ -24,6 +29,45 @@ func createTemplateCache(templateName string) error {
 	return nil
 }
 
+func createTemplateCacheEnhanced() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	/*
+		Get all the files named *.page.tmpl in ./templates
+	*/
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return cache, err
+	}
+
+	for _, page := range pages {
+		filename := filepath.Base(page)
+		template, err := template.New(filename).ParseFiles(page)
+		if err != nil {
+			return cache, err
+		}
+
+		layouts, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return cache, err
+		}
+
+		if len(layouts) > 0 {
+			template, err = template.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return cache, err
+			}
+		}
+
+		cache[filename] = template
+	}
+
+	return cache, nil
+}
+
+/*
+	Used before 3.30
+*/
 func RenderTemplate(w http.ResponseWriter, templateName string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/"+templateName, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
@@ -33,6 +77,9 @@ func RenderTemplate(w http.ResponseWriter, templateName string) {
 	}
 }
 
+/*
+	Used in 3.30
+*/
 func RenderTemplateWithCaching(w http.ResponseWriter, templateName string) {
 	var tmpl *template.Template
 	var err error
@@ -49,6 +96,29 @@ func RenderTemplateWithCaching(w http.ResponseWriter, templateName string) {
 
 	tmpl = templateCache[templateName]
 	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func RenderTemplateWithCachingEnhanced(w http.ResponseWriter, templateName string) {
+	templateCache, err := createTemplateCacheEnhanced()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	template, ok := templateCache[templateName]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+	err = template.Execute(buf, nil)
+	if !ok {
+		log.Fatal(err)
+	}
+
+	_, err = buf.WriteTo(w)
 	if err != nil {
 		log.Println(err)
 	}
