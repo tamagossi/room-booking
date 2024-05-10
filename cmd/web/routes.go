099148222118b1db44e8da1f3b3fb41bab510b7f
Raka Pratama <mgf.prauliyatama@gmail.com>
2024-05-10 10:58:12 +0700

    feat(4.39): configure `chi` router and comment `pat` router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,27 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/tamagossi/room-bookings/pkg/config"
 	"github.com/tamagossi/room-bookings/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	/*
+	 * Commented 4.39 to replace pat to use chi
+	   mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.HomeHandler))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.AboutHandler))
+	   mux.Get("/", http.HandlerFunc(handlers.Repo.HomeHandler))
+	   mux.Get("/about", http.HandlerFunc(handlers.Repo.AboutHandler))
+	*/
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.HomeHandler)
+	mux.Get("/about", handlers.Repo.AboutHandler)
 
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/tamagossi/room-bookings
 
 go 1.18
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+)
