12bc6d8deb93883a9f94ec37a2c1fa9fce96c14d
Raka Pratama <mgf.prauliyatama@gmail.com>
2024-05-11 21:30:46 +0700

    feat(6.45): config static files and add images in go

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,13 +19,15 @@ func routes(app *config.AppConfig) http.Handler {
 	*/
 
 	mux := chi.NewRouter()
-
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(LoadSession)
 
 	mux.Get("/", handlers.Repo.HomeHandler)
 	mux.Get("/about", handlers.Repo.AboutHandler)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
